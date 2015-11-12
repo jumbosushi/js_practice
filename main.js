3499a5c3321e327e4f200b097666bb988a0e596f
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2015-11-12 15:17:35 -0800

    learned about function scope

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-var a = Infinity/0;
-console.log(a);
-
+function test() {
+    var i = 1;
+    if (i == 1) {
+        var j = 2;
+        for (var k =0; k < 3; k++) {
+            console.log(k);
+        }
+    }
+    return k;
+}
