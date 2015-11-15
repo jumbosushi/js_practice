6516c7027db34b333bc4ea9b7830aa98b48ff34c
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2015-11-14 16:38:20 -0800

    Learned about function declaration and closures

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-function test() {
-    var i = 1;
-    if (i == 1) {
-        var j = 2;
-        for (var k =0; k < 3; k++) {
-            console.log(k);
-        }
-    }
-    return k;
+function makeAdder(x) {
+    return function(y) {
+        return x + y;
+    };
 }
+
+var add5 = makeAdder(5);
+var add10 = makeAdder(10);
+
+console.log(add5(2));
+console.log(add10(2));
+
