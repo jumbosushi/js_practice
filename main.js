0da748705dc0c8b5753e6cbe97a59e9eaa19a6fa
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2015-11-25 15:31:50 -0800

    Add js.issexy js tutorial Week 1 Dir

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
-function makeAdder(x) {
-    return function(y) {
-        return x + y;
-    };
+one = "Function Declaration";
+function x() {
+    console.log('x');
 }
 
-var add5 = makeAdder(5);
-var add10 = makeAdder(10);
+//processed before any step-by-step!
 
-console.log(add5(2));
-console.log(add10(2));
+y(); // Works even though it's above the declaration
+function y() {
+    console.log('x');
+}
 
+
+two = '"Anonymous" function Expression';
+var y = function() {
+    console.log('y');
+};
+
+//asigned a name of "y" in this case
+
+
+three = 'Named Function Expression';
+var z = function w() {
+    console.log('zw');
+};
+
+// Has a prper name "w"
+// The name of the function is not added to the scope in which the expression appreas
+// the name is in scope ithin the function itself.
+var z = function w() {
+    console.log(typeof w); //"function"
+};
+console.log(typeof w); // undefined
+
+
+four = "Accessor Function Initializer";
+var obj = {
+    value: 0,
+    get f() {
+        return this.value;
+    },
+    set f(v) {
+        this.value = v;
+    }
+};
+
+
+five = "Arrow Function Expression"
+
+var a = [1,2,3,];
+var b = a.map(n => n * 2);
